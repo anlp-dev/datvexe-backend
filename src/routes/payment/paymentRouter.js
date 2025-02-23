6970f6fb9146ecdddfd530f4cb9471cd9d2d2097
anlp-dev <anlp.sep19@gmail.com>
2025-02-23 16:50:22 +0700

    build api payment

--- src/routes/payment/paymentRouter.js
+++ src/routes/payment/paymentRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const PaymentController = require("../../controllers/payment/PaymentController");
 
 router.post("/create-url-vnpay", PaymentController.createPayment)
-router.post("/create-vietqr", PaymentController.createVietQr)
+// router.post("/create-vietqr", PaymentController.createVietQr)
 
 
 module.exports = router;
